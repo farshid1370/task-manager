3d73ff8ac56658d1f527db13f3c79cec956d9025
farshid1370 <farshid.amirkhani@gmail.com>
2019-05-17 19:06:41 +0430

    auto deploy test

--- src/routers/user.js
+++ src/routers/user.js
@@ -58,6 +58,11 @@ router.delete('/users/:id', async (seq, res) => {
         res.status(500).send()
     }
 })
+router.get('test',async(req,res)=>{
+    res.send({
+        test:'passed'
+    })
+})
 
 
 module.exports=router
